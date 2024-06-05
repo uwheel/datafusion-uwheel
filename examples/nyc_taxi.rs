1d1f16e87b86a379fe4a23430664f811560d5ed4
Max Meldrum <max@meldrum.se>
2024-06-05 15:28:47 +0200

    feat: add MinMax wheels

--- examples/nyc_taxi.rs
+++ examples/nyc_taxi.rs
@@ -1,12 +1,22 @@
-use chrono::DateTime;
-use wheel_manager::Builder;
+use chrono::{DateTime, NaiveDate, TimeZone, Utc};
+use uwheel::{wheels::read::ReaderWheel, WheelRange};
+use wheel_manager::{MinMaxAggregator, MinMaxState, WheelManager};
 
 fn main() {
-    let builder = Builder::new("data/", "tpep_dropoff_datetime")
-        .with_min_max_columns(vec!["fare_amount"])
+    // Build a WheelManager over parquet files in /data using the time column "tpep_dropoff_datetime"
+    let manager: WheelManager = wheel_manager::Builder::new("data/", "tpep_dropoff_datetime")
+        .with_min_max_wheels(vec!["fare_amount", "trip_distance"]) // Build Min/Max wheels for the columns "fare_amount" and "trip_distance"
         .build()
         .unwrap();
-    let count_wheel = builder.count_wheel();
+
+    // The following wheel can be used to quickly filter queries such as:
+    // SELECT * FROM yellow_tripdata
+    // WHERE tpep_dropoff_datetime >= '?' and < '?'
+    //
+    // Or can be used to quickly count the number of records between two dates
+    // SELECT COUNT(*) FROM yellow_tripdata
+    // WHERE tpep_dropoff_datetime >= '?' and < '?'
+    let count_wheel = manager.count_wheel();
 
     let watermark = count_wheel.watermark();
     println!("Landmark COUNT(*) {:?}", count_wheel.as_ref().landmark());
@@ -18,4 +28,58 @@ fn main() {
         .to_string();
 
     println!("WATERMARK: {:?}", watermark);
+
+    // The following wheel can be used to quickly filter queries such as:
+    // SELECT * FROM yellow_tripdata
+    // WHERE tpep_dropoff_datetime >= '?' and < '?'
+    // AND fare_amount > 1000
+    let fare_wheel = manager.min_max_wheel("fare_amount").unwrap();
+
+    // let's use the dates 2022-01-01 and 2022-01-10 to illustrate
+    let start = NaiveDate::from_ymd_opt(2022, 1, 1).unwrap();
+    let start_date = Utc.from_utc_datetime(&start.and_hms_opt(0, 0, 0).unwrap());
+
+    let end = NaiveDate::from_ymd_opt(2022, 1, 10).unwrap();
+    let end_date = Utc.from_utc_datetime(&end.and_hms_opt(0, 0, 0).unwrap());
+
+    // Whether there are fare amounts above 1000.0
+    let fare_pred = |min_max_state: MinMaxState| min_max_state.max_value() > 1000.0;
+
+    // Check whether the filter between 2022-01-01  and 2022-01-10 can be skipped
+    if temporal_filter(&fare_wheel, start_date, end_date, fare_pred) {
+        println!("Cannot skip execution since there are rides with fare_amount > 1000.0 between {:?} and {:?}", start_date, end_date);
+    } else {
+        println!("Skipping execution since there no rides with fare_amount > 1000.0 between {:?} and {:?}", start_date, end_date);
+    }
+
+    // Check whether the filter between 2022-01-01 12:30 and 12:50 can be skipped
+    let start = NaiveDate::from_ymd_opt(2022, 1, 1).unwrap();
+    let start_date = Utc.from_utc_datetime(&start.and_hms_opt(12, 30, 0).unwrap());
+
+    let end = NaiveDate::from_ymd_opt(2022, 1, 1).unwrap();
+    let end_date = Utc.from_utc_datetime(&end.and_hms_opt(12, 50, 0).unwrap());
+
+    if temporal_filter(&fare_wheel, start_date, end_date, fare_pred) {
+        println!("Cannot skip execution since there are rides with fare_amount > 1000.0 between {:?} and {:?}", start_date, end_date);
+    } else {
+        println!("Skipping execution since there no rides with fare_amount > 1000.0 between {:?} and {:?}", start_date, end_date);
+    }
+}
+
+// Executes a temporal wheel filter to determine whether a query can be skipped
+fn temporal_filter(
+    wheel: &ReaderWheel<MinMaxAggregator>,
+    start: DateTime<Utc>,
+    end: DateTime<Utc>,
+    predicate: impl Fn(MinMaxState) -> bool,
+) -> bool {
+    let start_ms = start.timestamp_millis() as u64;
+    let end_ms = end.timestamp_millis() as u64;
+
+    // Get the Min/Max state between the start and end date
+    let min_max_state = wheel
+        .combine_range_and_lower(WheelRange::new_unchecked(start_ms, end_ms))
+        .unwrap();
+
+    predicate(min_max_state)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,18 @@ use std::{
     collections::HashMap,
     fs::{DirEntry, File},
     path::{Path, PathBuf},
+    sync::{Arc, Mutex},
 };
 
 use chrono::{DateTime, NaiveDate, TimeZone, Utc};
-use datafusion::{arrow::array::TimestampMicrosecondArray, error::DataFusionError, prelude::*};
+use datafusion::{
+    arrow::{
+        array::{Float64Array, TimestampMicrosecondArray},
+        datatypes::DataType,
+    },
+    error::DataFusionError,
+    prelude::*,
+};
 use datafusion::{error::Result, parquet::arrow::arrow_reader::ParquetRecordBatchReaderBuilder};
 use uwheel::{
     aggregator::sum::U32SumAggregator,
@@ -13,8 +21,10 @@ use uwheel::{
     Conf, Entry, HawConf, RwWheel, WheelRange,
 };
 
-// column_name, aggregate function, data type
-// "fare_amount", sum, f32
+/// A MinMax Aggregator implementation
+pub mod min_max;
+
+pub use min_max::{MinMaxAggregator, MinMaxState};
 
 /// Builder for creating a WheelManager
 pub struct Builder {
@@ -43,8 +53,10 @@ impl Builder {
     }
 
     /// Columns to build min/max wheels for
-    pub fn with_min_max_columns(mut self, min_max_columns: Vec<&str>) -> Self {
-        self.min_max_columns = min_max_columns.iter().map(|s| s.to_string()).collect();
+    ///
+    /// Columns must be of numeric data types
+    pub fn with_min_max_wheels(mut self, columns: Vec<&str>) -> Self {
+        self.min_max_columns = columns.iter().map(|s| s.to_string()).collect();
         self
     }
     /// Pre-defined filters to apply to the parquet files
@@ -53,16 +65,13 @@ impl Builder {
         self
     }
     pub fn build(self) -> Result<WheelManager> {
-        WheelManager::try_new(self.dir, self.time_column)
+        WheelManager::try_new(self.dir, self.time_column, self.min_max_columns)
     }
 }
 
-/// Manages wheels over a set of parquet files
+/// A Wheel Manager over a set of parquet files
 ///
-/// Possible Wheels:
-/// - COUNT(*): U32
-/// - AGGREGATE(column): All
-/// - MIN/MAX(column):
+/// An extension that improves time-based analytical queries.
 pub struct WheelManager {
     /// The column in the parquet files that contains the time
     time_column: String,
@@ -70,27 +79,30 @@ pub struct WheelManager {
     dir: PathBuf,
     /// A COUNT(*) wheel over the parquet files and time column
     count: ReaderWheel<U32SumAggregator>,
-    // A Min/Max pruning wheels over the parquet files for a specific column
-    min_max_wheels: HashMap<String, ReaderWheel<U32SumAggregator>>,
-    // min_max_wheels: Vec<ReaderWheel<MinMaxAggregator>>,
+    // Min/Max pruning wheels over the parquet files for a specific column
+    min_max_wheels: Arc<Mutex<HashMap<String, ReaderWheel<MinMaxAggregator>>>>,
 }
 
 impl WheelManager {
     /// Create a new WheelManager using the given directory and time column
     ///
     /// If the specified time column does not exist in the parquet files, an error is returned.
-    pub fn try_new(dir: impl Into<PathBuf>, time_column: impl Into<String>) -> Result<Self> {
+    pub fn try_new(
+        dir: impl Into<PathBuf>,
+        time_column: impl Into<String>,
+        min_max_columns: Vec<String>,
+    ) -> Result<Self> {
         let dir = dir.into();
         let time_column = time_column.into();
 
         // Create an "initial" manager over the parquet files
-        let count = build(&dir, &time_column)?;
+        let (count, min_max_wheels) = build(&dir, &time_column, min_max_columns)?;
 
         Ok(Self {
             time_column,
             dir,
             count,
-            min_max_wheels: Default::default(),
+            min_max_wheels: Arc::new(Mutex::new(min_max_wheels)),
         })
     }
 
@@ -108,22 +120,24 @@ impl WheelManager {
         &self.count
     }
 
-    /// Return the minimum
-    ///
-    /// Returns `(None, None)` if the min/max is not available
-    pub fn min_max_wheel(&self, _column: usize) -> Option<ReaderWheel<U32SumAggregator>> {
-        // TODO: Implement min_max aggregation
-        None
+    /// Returns a reference to a MIN/MAX wheel for a specified column
+    pub fn min_max_wheel(&self, column: &str) -> Option<ReaderWheel<MinMaxAggregator>> {
+        self.min_max_wheels.lock().unwrap().get(column).cloned()
     }
-    //pub fn build_wheels(&self, _filters: &[Expr]) {}
-    //pub fn aggregate(&self, time: WheelRange, filter: &Expr) {}
 }
 
-fn build(dir: &PathBuf, time_column: &str) -> Result<ReaderWheel<U32SumAggregator>> {
+fn build(
+    dir: &PathBuf,
+    time_column: &str,
+    min_max_columns: Vec<String>,
+) -> Result<(
+    ReaderWheel<U32SumAggregator>,
+    HashMap<String, ReaderWheel<MinMaxAggregator>>,
+)> {
     let files = read_dir(dir.as_path())?;
 
     let mut timestamps = Vec::new();
-    for file in files {
+    for file in files.iter() {
         let file = File::open(file.path()).unwrap();
         let parquet_reader = ParquetRecordBatchReaderBuilder::try_new(file)?
             .with_batch_size(8192)
@@ -131,6 +145,8 @@ fn build(dir: &PathBuf, time_column: &str) -> Result<ReaderWheel<U32SumAggregato
 
         for batch in parquet_reader {
             let batch = batch?;
+
+            // Verify the time column exists in the parquet file
             let time_column_exists = batch.schema().index_of(time_column).is_ok();
             assert!(
                 time_column_exists,
@@ -150,21 +166,116 @@ fn build(dir: &PathBuf, time_column: &str) -> Result<ReaderWheel<U32SumAggregato
     }
 
     // Build a COUNT(*) wheel over the timestamps
-    let count_wheel = build_count_wheel(timestamps);
+    let (count_wheel, min_timestamp_ms, max_timestamp_ms) = build_count_wheel(timestamps);
 
-    // TODO: Build min/max wheels for specified columns
+    // Build MinMax wheels for specified columns
+    let mut map = HashMap::new();
+    for column in min_max_columns.iter() {
+        let min_max_wheel = build_min_max_wheel(
+            &files,
+            min_timestamp_ms,
+            max_timestamp_ms,
+            time_column,
+            column,
+        )?;
+        map.insert(column.to_string(), min_max_wheel);
+    }
 
-    Ok(count_wheel.read().clone())
+    Ok((count_wheel.read().clone(), map))
+}
+
+fn build_min_max_wheel(
+    files: &[DirEntry],
+    _min_timestamp_ms: u64,
+    max_timestamp_ms: u64,
+    time_col: &str,
+    min_max_col: &str,
+) -> Result<ReaderWheel<MinMaxAggregator>> {
+    // TODO: remove hardcoded time
+    let start = NaiveDate::from_ymd_opt(2022, 1, 1).unwrap();
+    let date = Utc.from_utc_datetime(&start.and_hms_opt(0, 0, 0).unwrap());
+    let start_ms = date.timestamp_millis() as u64;
+
+    // TODO: get Conf from builder
+    let mut conf = HawConf::default()
+        .with_watermark(start_ms)
+        .with_mode(WheelMode::Index);
+
+    conf.minutes
+        .set_retention_policy(uwheel::RetentionPolicy::Keep);
+
+    conf.hours
+        .set_retention_policy(uwheel::RetentionPolicy::Keep);
+
+    conf.days
+        .set_retention_policy(uwheel::RetentionPolicy::Keep);
+
+    conf.weeks
+        .set_retention_policy(uwheel::RetentionPolicy::Keep);
+
+    let mut wheel: RwWheel<MinMaxAggregator> = RwWheel::with_conf(
+        Conf::default()
+            .with_haw_conf(conf)
+            .with_write_ahead(64000usize.next_power_of_two()),
+    );
+
+    for file in files.iter() {
+        let file = File::open(file.path()).unwrap();
+        let parquet_builder = ParquetRecordBatchReaderBuilder::try_new(file)?.with_batch_size(8192);
+
+        let schema = parquet_builder.schema().clone();
+        let parquet_reader = parquet_builder.build()?;
+
+        let column_index = schema.index_of(min_max_col)?;
+        let column_field = schema.field(column_index);
+
+        if is_numeric_type(column_field.data_type()) {
+            for batch in parquet_reader {
+                let batch = batch?;
+
+                let time_array = batch
+                    .column_by_name(time_col)
+                    .unwrap()
+                    .as_any()
+                    .downcast_ref::<TimestampMicrosecondArray>()
+                    .unwrap();
+
+                let min_max_column_array = batch
+                    .column_by_name(min_max_col)
+                    .unwrap()
+                    .as_any()
+                    .downcast_ref::<Float64Array>()
+                    .unwrap();
+
+                for (timestamp, value) in time_array
+                    .values()
+                    .iter()
+                    .copied()
+                    .zip(min_max_column_array.values().iter().copied())
+                {
+                    let timestamp_ms = DateTime::from_timestamp_micros(timestamp as i64)
+                        .unwrap()
+                        .timestamp_millis() as u64;
+                    let entry = Entry::new(value, timestamp_ms);
+                    wheel.insert(entry);
+                }
+            }
+            wheel.advance_to(max_timestamp_ms);
+        } else {
+            panic!("Min/Max column must be a numeric type");
+        }
+    }
+    Ok(wheel.read().clone())
 }
 
 /// Builds a COUNT(*) wheel over the given timestamps
 ///
 /// Uses a U32SumAggregator internally with prefix-sum optimization
-fn build_count_wheel(timestamps: Vec<i64>) -> RwWheel<U32SumAggregator> {
+fn build_count_wheel(timestamps: Vec<i64>) -> (RwWheel<U32SumAggregator>, u64, u64) {
     let min = timestamps.iter().min().copied().unwrap();
     let max = timestamps.iter().max().copied().unwrap();
 
-    let _min_ms = DateTime::from_timestamp_micros(min)
+    let min_ms = DateTime::from_timestamp_micros(min)
         .unwrap()
         .timestamp_millis() as u64;
 
@@ -214,7 +325,7 @@ fn build_count_wheel(timestamps: Vec<i64>) -> RwWheel<U32SumAggregator> {
     count_wheel.read().to_simd_wheels();
     count_wheel.read().to_prefix_wheels();
 
-    count_wheel
+    (count_wheel, min_ms, max_ms)
 }
 
 /// Return a list of the directory entries in the given directory, sorted by name
@@ -231,4 +342,21 @@ fn read_dir(dir: &Path) -> Result<Vec<DirEntry>> {
         .collect::<Result<Vec<DirEntry>>>()?;
     files.sort_by_key(|entry| entry.file_name());
     Ok(files)
+}
+
+fn is_numeric_type(data_type: &DataType) -> bool {
+    matches!(
+        data_type,
+        DataType::Int8
+            | DataType::Int16
+            | DataType::Int32
+            | DataType::Int64
+            | DataType::UInt8
+            | DataType::UInt16
+            | DataType::UInt32
+            | DataType::UInt64
+            | DataType::Float16
+            | DataType::Float32
+            | DataType::Float64
+    )
 }
