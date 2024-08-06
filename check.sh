cfaabbbad376c88452dce04096d4d908b7a15f12
Max Meldrum <max@meldrum.se>
2024-08-06 14:55:49 +0200

    chore: add local check script

---
+++ check.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -eux
+
+cargo +stable install cargo-hack --locked
+
+cargo hack check --all
+cargo fmt --all -- --check
+cargo hack clippy --workspace --all-targets --  -D warnings -W clippy::all
+cargo hack test --workspace
